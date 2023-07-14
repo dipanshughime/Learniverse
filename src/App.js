3430bfefa575576b6bdeec8ad297ba0bae863d9c
Dipanshu ghime <dipanshughime@gmail.com>
2023-07-14 18:59:13 +0530

    started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="navbar">
+        <div className="navbar-left">
+          <h1 className="logo">Leaniverse</h1>
+        </div>
+        <div className="navbar-right">
+          <button className="button">Login</button>
+          <button className="button">Sign Up</button>
+        </div>
+      </nav>
+      {/* Rest of your application */}
     </div>
   );
 }
